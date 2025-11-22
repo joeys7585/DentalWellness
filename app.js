5fad819d6a1a7a14a44905dcc0b34c2387a42b0d
joeys7585 <joecasillas001@gmail.com>
2025-11-22 16:31:48 +0530

    Dental Wellness build 1

---
+++ app.js
@@ -0,0 +1,30 @@
+let menu = document.querySelector(".menu-icon");
+let navBar = document.querySelector(".navbar");
+
+menu.onclick = () => {
+	menu.classList.toggle("move");
+	navBar.classList.toggle("open-menu");
+};
+var swiper = new Swiper(".services-content", {
+	spaceBetween: 30,
+	centeredSlides: true,
+	autoplay: {
+		delay: 5000,
+		disableOnInteraction: false,
+	},
+	navigation: {
+		nextEl: ".swiper-button-next",
+		prevEl: ".swiper-button-prev",
+	},
+});
+
+let header = document.querySelector("header");
+window.addEventListener("scroll", () => {
+	header.classList.toggle("shadow", window.scrollY > 0);
+});
+onload = () => {
+	const load = document.getElementById("loader");
+	setTimeout(() => {
+		load.style.display = "none";
+	}, 2500);
+};
